a10e0ea36c816c935fb8fb66ad9493c7f476e480
Vilhelm Prytz <vilhelm@prytznet.se>
2019-08-13 13:37:38 +0200

    Remove table on launch

---
+++ site/remove_tables.py
@@ -0,0 +1,4 @@
+from db import sql_query
+
+sql_query("DROP TABLE bookings")
+sql_query("DROP TABLE bc_bookings")
--- site/start.sh
+++ site/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd /var/www/booking.vilhelmprytz.se/deploy
+
+python remove_tables.py
+
 /usr/bin/env gunicorn --workers=5 --bind 127.0.0.1:7500 app:app
