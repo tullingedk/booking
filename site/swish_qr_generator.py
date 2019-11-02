f6aee082c0a4219a25804f4b8231e0d85040e4d4
Vilhelm Prytz <vilhelm@prytznet.se>
2019-11-02 19:46:04 +0100

    Fix Swish QR code text showing with encoding sign b'
    Text now appears as it should

--- site/swish_qr_generator.py
+++ site/swish_qr_generator.py
@@ -33,7 +33,7 @@ def generate_swish_qr(firstname, lastname, school_class, booking_type):
         border = 4,
     )
 
-    data = "C+46730333185;{};{}+{}+{};0".format(str(amount), firstname.encode('utf-8'), lastname.encode('utf-8'), school_class.encode('utf-8'))
+    data = "C+46730333185;{};{}+{}+{};0".format(str(amount), firstname, lastname, school_class)
 
     print(data)
 
