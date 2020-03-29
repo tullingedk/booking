62481b7c846fd8b2aa44143588ff95681dce661b
Vilhelm Prytz <vilhelm@prytznet.se>
2020-03-29 20:35:19 +0200

    script: get list of all students from Vklass

---
+++ backend/scripts/vklass_get_all_students_to_csv.py
@@ -0,0 +1,44 @@
+import requests
+
+s = requests.Session()
+
+login = {
+    "Username": "",
+    "Password": ""
+}
+
+s.post("https://auth.vklass.se/credentials/signin", data = login)
+r_class_list = s.get("https://www.vklass.se/SchoolClasses.aspx?id=b5bf211e-414f-4403-8321-e1a5569500e1")
+
+def every_student(s, student_url, class_name):
+    email = None
+    name = None
+    
+    r = s.get(f"https://www.vklass.se/{student_url}")
+    for line in r.text.split("\n"):
+        if "ctl00_ContentPlaceHolder2_nameLabel" in line:
+            name = line.split("</b>")[1][1:].split("</span>")[0]
+        if "ctl00_ContentPlaceHolder2_mailLabel" in line:
+            try:
+                temp = line.split("</b>")[1]
+                name = temp
+            except Exception:
+                pass
+
+    print(f"{name}, {email}, {class_name}")
+            
+def every_class(s, class_name, class_url):
+    r = s.get(f"https://www.vklass.se/{class_url}")
+    for line in r.text.split("\n"):
+        if "ctl00_ContentPlaceHolder2_studentRepeater_" in line:
+            student_url = line.split('href="')[1].split('">')[0]
+            every_student(s, student_url, class_name)
+
+
+for line_class_list in r_class_list.text.split("\n"):
+    if "ctl00_ContentPlaceHolder2_classRepeater_" in line_class_list:
+        class_url = line_class_list.split('href="')[1].split("&amp;")[0]
+        class_name = line_class_list.split('">')[2].split("</a></td>")[0]
+
+        every_class(s, class_name, class_url)
+
