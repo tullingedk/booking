d4cc502c33c075fe76ef8dad968e34a95896f6fa
Vilhelm Prytz <vilhelm@prytznet.se>
2020-09-02 19:35:10 +0200

    Travis: make sure deploy only runs once

--- scripts/travis_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t tullingedk/booking:latest backend
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && docker push tullingedk/booking:latest
